4b62b627d797c38da8c99133ff297325eb5e8498
Unknown <nis16104@uvg.edu.gt>
2019-09-15 12:17:23 -0600

    mas dry

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ class Player:
     def __init__(self, socket, user_name):
         self.socket = socket
         self.user_name = user_name
-        self.state = 'chilling'
+        self.state = 'chilling' #inroom - playing
 
 class Room:
     def __init__(self, room_name):
@@ -18,16 +18,16 @@ class Room:
         self.table = None
         self.players = []
         self.deck = None
-        self.state = 'waiting'
+        self.state = 'waiting' #going
 
 class Server:
     def __init__(self, host='127.0.0.1', port=3000):
+        print("listening on", (host, port))
         self.sel = selectors.DefaultSelector()
         self.lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.lsock.bind((host, port))
         self.lsock.listen()
-        print("listening on", (host, port))
         self.lsock.setblocking(False)
         self.sel.register(self.lsock, selectors.EVENT_READ, data=None)
         self.players = {}
@@ -39,12 +39,14 @@ class Server:
         conn.setblocking(False)
         socket = lib.SocketHandler(self.sel, conn, addr)
         self.sel.register(conn, selectors.EVENT_READ, data=socket)
+
     
     def get_rooms(self):
         rooms = []
         for name in self.rooms.keys():
             rooms.append(name)
         return self.rooms
+
     
     def get_players(self):
         players = []
@@ -52,47 +54,61 @@ class Server:
             players.append(name)
         return players
     
+
     def evaluate_request(self, socket):
-        addr = socket.addr
         request = socket.request
-        print(request)
-        print(addr)
-        if request['action'] == 'login':
-            value = request['value']
+        print(socket.addr, request)
+        close = False
+        value = request['value']
+        action = request['action']
+        if action == 'login':
+            print("agregar usuario")
             user = Player(socket, value)
             if value not in self.players.keys():
                 self.players[value] = user
                 print("usuario agregado")
                 content = {"status":"ok", "rooms":self.get_rooms(), "players": self.get_players()}
-                socket.write(content)
             else:
                 print("usuario ya existe")
-                name = value
-                content = {"status":"error", "message": f'User "{name}" exists'}
-                socket.write(content)
-                socket.close()
-        elif request['action'] == 'get_rooms':
-            content = {"status":"ok", "rooms":self.get_rooms()}
-            socket.write(content)
+                content = {"status":"error", "message": f'User "{value}" exists'}
+                close = True
+
+        elif action == 'get_rooms':
             print("mostrar rooms")
-        elif request['action'] == 'create_room':
+            content = {"status":"ok", "rooms":self.get_rooms()}
+
+        elif action == 'create_room':
             print("agregar room")
-        elif request['action'] == 'join_room':
+            room = Room(value)
+            if value not in self.rooms.keys():
+                self.rooms[value] = room
+                print("room agregado")
+                content = {"status":"ok", "rooms":self.get_rooms()}
+            else:
+                print("room ya existe")
+                content = {"status":"error", "message": f'Room "{value}" exists'}
+
+        elif action == 'join_room':
             print("agregar a room")
             print("agregar cambiar estado")
-        elif request['action'] == 'get_players':
+
+        elif action == 'get_players':
             print("mostrar jugadores")
             content = {"status":"ok", "players": self.get_players()}
-            socket.write(content)
-        elif request['action'] == 'disconnect':
+
+        elif action == 'disconnect':
             print("bye player")
             content = {"status":"ok"}
-            socket.write(content)
-            socket.close()
+            close = True
+
         else:
-            action = request['action']
             content = {"status":"error", "message": f'Command "{action}" doesnt exists'}
-            socket.write(content)
+
+        socket.write(content)
+        if close:
+            socket.close()
+    
+
     def start(self):
         try:
             while True:
@@ -103,7 +119,6 @@ class Server:
                     else:
                         socket = key.data
                         try:
-                            print("mask:",mask)
                             if mask & selectors.EVENT_READ:
                                 socket.read()
                             if mask & selectors.EVENT_WRITE:
@@ -117,7 +132,7 @@ class Server:
         except KeyboardInterrupt:
             print("caught keyboard interrupt, exiting")
         finally:
-           self. sel.close()
+           self.sel.close()
 
 
 server = Server()
