770dffb22276250529e7458a441f9888b715d62b
Gadhi <pablogadhi@gmail.com>
2019-09-18 15:00:51 -0600

    More fixes

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ class Server:
         self.sel.register(self.lsock, selectors.EVENT_READ, data=None)
         self.players = {}
         self.rooms = {}
-    
+
     def accept_wrapper(self, sock):
         conn, addr = sock.accept()  # Should be ready to read
         print("accepted connection from", addr)
@@ -26,34 +26,31 @@ class Server:
         socket = lib.SocketHandler(self.sel, conn, addr)
         self.sel.register(conn, selectors.EVENT_READ, data=socket)
 
-    
     def get_rooms(self):
         rooms = []
         for name in self.rooms.keys():
             rooms.append(name)
         return rooms
 
-    
     def get_players(self):
         players = []
         for name in self.players.keys():
             players.append(name)
         return players
-    
+
     def message_to_room(self, content, room):
         room = self.rooms[room]
         if room:
             room.send_message_to_all(content)
             return True
         return False
-    
+
     def message_to_player(self, content, player):
         player = self.players[player]
         if player:
             player.write(content)
             return True
         return False
-    
 
     def evaluate_request(self, socket):
         request = socket.request
@@ -68,16 +65,18 @@ class Server:
             if user not in self.players.keys():
                 self.players[user] = player
                 print("usuario agregado")
-                content = {"status":"login", "rooms":self.get_rooms(), "players": self.get_players()}
+                content = {"status": "login", "rooms": self.get_rooms(
+                ), "players": self.get_players()}
             else:
                 print("usuario ya existe")
-                content = {"status":"error", "message": f'User "{user}" exists'}
+                content = {"status": "error",
+                           "message": f'User "{user}" exists'}
                 close = True
 
         elif user in self.players.keys():
             if action == 'get_rooms':
                 print("mostrar rooms")
-                content = {"status":"get_rooms", "rooms":self.get_rooms()}
+                content = {"status": "get_rooms", "rooms": self.get_rooms()}
 
             elif action == 'create_room':
                 print("agregar room")
@@ -86,81 +85,95 @@ class Server:
                 if room_name not in self.rooms.keys():
                     self.rooms[room_name] = room
                     print("room agregado")
-                    content = {"status":"room_created", "rooms":self.get_rooms()}
+                    content = {"status": "room_created",
+                               "rooms": self.get_rooms()}
                 else:
                     print("room ya existe")
-                    content = {"status":"error", "message": f'Room "{room_name}" exists'}
+                    content = {"status": "error",
+                               "message": f'Room "{room_name}" exists'}
 
             elif action == 'join_room':
                 print("usuario unido a room")
                 room_name = request['room']
                 if room_name in self.rooms.keys():
-                    connected = self.rooms[room_name].connect_player(self.players[user])
+                    connected = self.rooms[room_name].connect_player(
+                        self.players[user])
                     if connected:
                         self.players[user].change_state('inroom')
-                        content = {"status":"join_room", "room":room_name ,"players_in_room": self.rooms[room_name].get_players_in_room()}
+                        content = {"status": "join_room", "room": room_name,
+                                   "players_in_room": self.rooms[room_name].get_players_in_room()}
                         socket.write(content)
                         write = False
                         self.rooms[room_name].try_start()
                     else:
-                        content = {"status":"error", "message": f'Room "{room_name}"is full'}
+                        content = {"status": "error",
+                                   "message": f'Room "{room_name}"is full'}
 
                 else:
-                    content = {"status":"error", "message": f'Room "{room_name}"doesnt exists'}
+                    content = {"status": "error",
+                               "message": f'Room "{room_name}"doesnt exists'}
 
             elif action == 'get_players':
                 print("mostrar jugadores")
-                content = {"status":"get_players", "players": self.get_players()}
-            
+                content = {"status": "get_players",
+                           "players": self.get_players()}
+
             elif action == 'message_to_room':
                 print("enviar mensaje a room")
                 room = request['room']
                 message = request['message']
-                content = {"status":"message_to_room", "from":user , "room":room, "message": message}
+                content = {"status": "message_to_room",
+                           "from": user, "room": room, "message": message}
                 if(self.message_to_room(content, room)):
-                    content = {"status":"message_to_room_sent", "from":user , "room":room, "message": message}
+                    content = {"status": "message_to_room_sent",
+                               "from": user, "room": room, "message": message}
                 else:
-                    content = {"status":"error", "message": 'couldnt send message to room'}
-            
+                    content = {"status": "error",
+                               "message": 'couldnt send message to room'}
+
             elif action == 'message_to_player':
                 print("enviar mensaje a jugador")
                 to = request['to']
                 message = request['message']
-                content = {"status":"message_to_player", "from":user , "to":to, "message": message}
+                content = {"status": "message_to_player",
+                           "from": user, "to": to, "message": message}
                 if (self.message_to_player(content, to)):
-                    content = {"status":"message_to_player_sent", "from":user , "to":to, "message": message}
+                    content = {"status": "message_to_player_sent",
+                               "from": user, "to": to, "message": message}
                 else:
-                    content = {"status":"error", "message": 'couldnt send message to player'}
+                    content = {"status": "error",
+                               "message": 'couldnt send message to player'}
 
             elif action == 'card_pick':
                 print('seleccion del jugador')
                 room = request['room']
                 pick = request['card']
-                player_name = request['player_name']
+                player_name = user
                 if room in self.rooms.keys():
-                    self.rooms[room].card_pick(card, player_name)
+                    self.rooms[room].card_pick(pick, player_name)
                     write = False
                 else:
-                    content = {"status":"error", "message": 'room doesnt exists'}
+                    content = {"status": "error",
+                               "message": 'room doesnt exists'}
 
             elif action == 'disconnect':
                 print("bye player")
                 self.players.pop(user)
-                content = {"status":"disconnect"}
+                content = {"status": "disconnect"}
                 close = True
 
-            #TODO: enviar y recibir mensajes (chat)
-            #TODO: logica del juego
+            # TODO: enviar y recibir mensajes (chat)
+            # TODO: logica del juego
             else:
-                content = {"status":"error", "message": f'Command "{action}" doesnt exists'}
+                content = {"status": "error",
+                           "message": f'Command "{action}" doesnt exists'}
         else:
-                content = {"status":"error", "message": 'login first'}
+            content = {"status": "error", "message": 'login first'}
 
         if write:
             socket.write(content)
         if close:
             socket.close()
-    
 
     def start(self):
         try:
@@ -185,9 +198,8 @@ class Server:
         except KeyboardInterrupt:
             print("caught keyboard interrupt, exiting")
         finally:
-           self.sel.close()
+            self.sel.close()
 
 
 server = Server()
 server.start()
-
